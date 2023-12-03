339c309c8b3b2e43b700008d7dfb7dfeff3afe3e
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-02 20:41:36 -0800

    correct list

--- app/boards/[board]/page.tsx
+++ app/boards/[board]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Image from "next/image";
 
@@ -43,10 +43,18 @@ export default function Page({
 }: {
   params: { board: string };
 }) {
-  console.log(params);
-
   const [images, setImages] = useState<
     { alt: string; src: string }[]
+  >([]);
+
+  const [selectedOne, setSelectedOne] = useState<
+    number | null
+  >(null);
+  const [selectedTwo, setSelectedTwo] = useState<
+    number | null
+  >(null);
+  const [correctImages, setCorrectImages] = useState<
+    number[]
   >([]);
 
   const shuffle = (
@@ -57,27 +65,85 @@ export default function Page({
       const j = Math.floor(Math.random() * (i + 1));
       [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
     }
-    setImages(shuffle);
-    return newArr;
+    setImages(newArr);
   };
+
+  useEffect(() => {
+    shuffle(mockImages);
+  }, []);
+  useEffect(() => {
+    if (selectedOne && selectedTwo) {
+      if (
+        images[selectedOne].src === images[selectedTwo].src
+      ) {
+        console.log("matched!");
+        setCorrectImages([
+          ...correctImages,
+          selectedOne,
+          selectedTwo,
+        ]);
+      } else {
+        console.log("incorrect!");
+      }
+
+      setSelectedOne(null);
+      setSelectedTwo(null);
+      if (correctImages.length === 16) {
+        console.log("you won!");
+      }
+    }
+  }, [selectedOne, selectedTwo]);
+
+  const handleImageClick = () => {};
 
   return (
     <main className='container mx-auto w-full h-screen flex-col justify-center items-center border'>
       <h1>Board title</h1>
       <section>
         <div className='w-full h-full grid grid-cols-4'>
-          {mockImages.map(({ alt, src }) => {
-            return (
-              <div key={alt}>
-                <Image
-                  src={src}
-                  alt={alt}
-                  width={50}
-                  height={50}
-                />
-              </div>
-            );
-          })}
+          {images.length > 0 &&
+            images.map(({ alt, src }, index) => {
+              const correct = correctImages.includes(index);
+              console.log("correct", correct);
+              console.log("correct list", correctImages);
+
+              const selected =
+                selectedOne === index ||
+                selectedTwo === index
+                  ? "border border-rose-500"
+                  : "";
+
+              return (
+                <button
+                  disabled={correct}
+                  className={`relative ${selected} ${
+                    correct && ""
+                  }`}
+                  key={index}
+                  onClick={() =>
+                    !selectedOne && !selectedTwo
+                      ? setSelectedOne(index)
+                      : setSelectedTwo(index)
+                  }
+                >
+                  <Image
+                    src={src}
+                    alt={alt}
+                    width={50}
+                    height={50}
+                  />
+                  <div
+                    className={`${
+                      correct
+                        ? "absolute bg-gray-400 bg-opacity-75 inset-0 w-full h-full"
+                        : "hidden"
+                    }`}
+                  >
+                    X
+                  </div>
+                </button>
+              );
+            })}
         </div>
       </section>
     </main>
