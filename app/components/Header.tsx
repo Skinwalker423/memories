23b9f8bf4a1563aac90adbef730d00b9da1ab1f1
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-04 17:49:34 -0800

    nav bar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className='w-full'>
-      <div className='flex justify-between items-center w-full'>
-        <div>Icon</div>
-        <div>
-          <div>Boards</div>
+    <header className=' w-full px-10 bg-slate-400'>
+      <nav className='flex justify-between items-center w-full py-4'>
+        <a href='/'>Logo</a>
+        <div className='flex gap-3'>
+          <a href={"/boards"}>Boards</a>
           <div>Sign In</div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
