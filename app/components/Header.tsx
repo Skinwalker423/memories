a4e8a209d51c617f3764099b26ca6a816be87eb9
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-04 17:13:31 -0800

    header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='w-full'>
+      <div className='flex justify-between items-center w-full'>
+        <div>Icon</div>
+        <div>
+          <div>Boards</div>
+          <div>Sign In</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
