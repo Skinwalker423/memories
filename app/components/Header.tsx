caf6194882664875a6835bebd04ce278f31561f2
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-06 19:00:13 -0800

    navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,23 @@
 "use client";
 
+import { Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 
-const Header = () => {
+const Header = ({
+  userSession,
+}: {
+  userSession: Session | null;
+}) => {
   const session = useSession();
-  console.log(session);
+  console.log("user session", userSession);
   return (
-    <header className=' w-full px-10 bg-slate-400'>
+    <header className=' w-full px-10 nav-gradient'>
       <nav className='flex justify-between items-center w-full py-4'>
         <a href='/'>Logo</a>
-        <div className='flex gap-3'>
+        <div className='flex gap-3 items-center'>
           <a href={"/boards"}>Boards</a>
-          {!session.data ? (
+          {!session.data && !userSession ? (
             <button
               className='bg-green-400 px-4 py-2 rounded-xl hover:bg-green-600'
               onClick={() => signIn()}
