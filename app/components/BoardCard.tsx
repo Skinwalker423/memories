9fea0b48b4c0c9e9ddab8866b3ebad63e6295b86
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-07 17:40:29 -0800

    boards

--- app/components/BoardCard.tsx
+++ app/components/BoardCard.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 const BoardCard = () => {
-  return <div>BoardCard</div>;
+  return (
+    <div className='max-w-2xl h-auto'>
+      <Image
+        src={"/images/johnwick.jpg"}
+        alt='john wick'
+        className='object-contain w-10 h-10'
+        fill
+      />
+    </div>
+  );
 };
 
 export default BoardCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+import BoardCard from "./components/BoardCard";
 import Hero from "./components/Hero";
+
+const boards = [
+  {
+    title: "John Wick Theme",
+    id: 0,
+    image: "/images/johnwick.jpg",
+  },
+  {
+    title: "Harry Potter Theme",
+    id: 1,
+    image: "/images/fakeavatar.jpg",
+  },
+  {
+    title: "Delicious Deserts Theme",
+    id: 2,
+    image: "/images/cheesecake.jpg",
+  },
+];
 
 export default async function Home() {
   return (
     <main className='w-full h-full justify-center'>
       <Hero />
+      {boards.map(({ title, id, image }) => {
+        return <BoardCard />;
+      })}
     </main>
   );
 }
