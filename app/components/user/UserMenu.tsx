f44fa5e51891767be0aeb63ee8d09894615ecaf4
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-29 10:23:49 -0700

    UserMenu

---
+++ app/components/user/UserMenu.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+const UserMenu = () => {
+  const session = useSession();
+  return (
+    <div className='flex gap-3 items-center z-50'>
+      {!session.data ? (
+        <button
+          className='bg-yellow-500 px-2 py-1 rounded-xl hover:bg-yellow-400'
+          onClick={() => signIn()}
+        >
+          Sign In
+        </button>
+      ) : (
+        <button
+          className='bg-neutral-600 px-2 py-1 rounded-xl text-white hover:bg-red-500'
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
