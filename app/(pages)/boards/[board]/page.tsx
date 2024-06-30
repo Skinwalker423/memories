8d29052b1006c5ab28a7a0d64d957c81da238c36
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-30 11:53:53 -0700

    pages folder

--- app/(pages)/boards/[board]/page.tsx
+++ app/(pages)/boards/[board]/page.tsx
@@ -1,22 +1,29 @@
-import { fetchBoardById } from "@/app/actions/memory_boards";
-import { MemoryGame } from "@/app/components/memory_game/MemoryGame";
 import { notFound } from "next/navigation";
+
+import { fetchBoardById } from "@/app/actions/memory_boards";
+
+import { MemoryGame } from "@/app/components/memory_game/MemoryGame";
+import { mockImages } from "@/app/constants";
 
 export default async function BoardPage({
   params,
 }: {
   params: { board: string };
 }) {
-  const board = await fetchBoardById(params.board);
-  console.log("board", board?.images);
+  const board =
+    process.env.NODE_ENV === "production"
+      ? await fetchBoardById(params.board)
+      : mockImages;
 
   if (!board) return notFound();
 
   if (board.images.length < 8)
     return <p>Not Enough images to form a game </p>;
 
-  <MemoryGame
-    boardImages={board.images}
-    title={board.title}
-  />;
+  return (
+    <MemoryGame
+      boardImages={board.images}
+      title={board.title}
+    />
+  );
 }
