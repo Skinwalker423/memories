eaead46287eaa960c1f91cc6b9911d20eccdcddc
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-30 11:52:20 -0700

    mock data

---
+++ app/constants/index.ts
@@ -0,0 +1,13 @@
+export const mockImages = {
+  title: "Test Board",
+  images: [
+    "/images/johmwick.jpg",
+    "/images/cheesecake.jpg",
+    "/images/curry.jpg",
+    "/images/skinwalker.jpg",
+    "/images/brisket.jpeg",
+    "/images/brisket.jpeg",
+    "/images/avatar.webp",
+    "/images/ramen.jpg",
+  ],
+};
