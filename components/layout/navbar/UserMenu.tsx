2ddb3a4e5a071ac598c465d0e3e8e69862cdf46d
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-04 09:18:37 -0700

    User menu dropdown

--- components/layout/navbar/UserMenu.tsx
+++ components/layout/navbar/UserMenu.tsx
@@ -1,26 +1,56 @@
-"use client";
+import { auth, signOut } from "@/auth";
+import { SignIn } from "@/components/auth/SignIn";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import { Lightbulb, LogOut, Settings } from "lucide-react";
+import Link from "next/link";
 
-import { signIn, signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-
-export const UserMenu = () => {
-  const session = useSession();
+export const UserMenu = async () => {
+  const session = await auth();
   return (
     <div className='flex gap-3 items-center z-50'>
-      {!session.data ? (
-        <button
-          className='bg-yellow-500 px-2 py-1 rounded-xl hover:bg-yellow-400'
-          onClick={() => signIn()}
-        >
-          Sign In
-        </button>
+      {!session?.user ? (
+        <SignIn />
       ) : (
-        <button
-          className='bg-neutral-600 px-2 py-1 rounded-xl text-white hover:bg-red-500'
-          onClick={() => signOut()}
-        >
-          Sign Out
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src={session.user.image || ""} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='w-56' align='end'>
+            <DropdownMenuLabel>
+              My Account
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <Link href={"/"}>
+              <DropdownMenuItem className='flex justify-start items-center gap-3'>
+                <Lightbulb /> <span>Create Memory</span>
+              </DropdownMenuItem>
+            </Link>
+            <DropdownMenuItem className='flex justify-start items-center gap-3'>
+              <Settings /> <span>Settings</span>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className='bg-destructive text-destructive-foreground flex justify-start items-center gap-3 hover:bg-destructive/80'>
+              <LogOut />
+              <span>Sign out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
     </div>
   );
