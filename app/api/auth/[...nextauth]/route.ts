95aae950146aba2c8eb2323013872ba1706d87e8
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-04 08:06:03 -0700

    next-auth beta

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,29 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async session({ session, token, user }) {
-      console.log("session", session);
-      return session;
-    },
-  },
-});
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
 
-export { handler as GET, handler as POST };
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//       authorization: {
+//         params: {
+//           prompt: "consent",
+//           access_type: "offline",
+//           response_type: "code",
+//         },
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ session, token, user }) {
+//       console.log("session", session);
+//       return session;
+//     },
+//   },
+// });
+
+// export { handler as GET, handler as POST };
