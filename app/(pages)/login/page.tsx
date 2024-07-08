4a5ad9818f8153a06217ba3a071e05ab6b325f58
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-08 11:37:03 -0700

    Login page redirect

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,24 +1,37 @@
 import { login, signup } from "@/app/actions/users";
+import { GoogleButton } from "@/components/auth/GoogleButton";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = createClient();
+
+  const isLoggedIn = (await supabase.auth.getUser()).data
+    .user;
+
+  if (isLoggedIn) redirect("/");
+
   return (
-    <form>
-      <label htmlFor='email'>Email:</label>
-      <input
-        id='email'
-        name='email'
-        type='email'
-        required
-      />
-      <label htmlFor='password'>Password:</label>
-      <input
-        id='password'
-        name='password'
-        type='password'
-        required
-      />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div>
+      <form>
+        <label htmlFor='email'>Email:</label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          required
+        />
+        <label htmlFor='password'>Password:</label>
+        <input
+          id='password'
+          name='password'
+          type='password'
+          required
+        />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+      <GoogleButton />
+    </div>
   );
 }
