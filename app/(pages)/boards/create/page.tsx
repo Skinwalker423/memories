b27ce45942c0eb1dbf8ea014eff462d3148b168d
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-08 11:51:11 -0700

    create board page redirect if not logged in

--- app/(pages)/boards/create/page.tsx
+++ app/(pages)/boards/create/page.tsx
@@ -1,6 +1,16 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const BoardCreatePage = () => {
+const BoardCreatePage = async () => {
+  const supabase = createClient();
+
+  const response = await supabase.auth.getUser();
+
+  const isLoggedIn = response.data.user;
+
+  if (!isLoggedIn) redirect("/login");
+
   return <div>BoardCreatePage</div>;
 };
 
