93af2dc59f76504c7152c50c4f84047eddebfb2d
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-08 11:42:14 -0700

    updated signout

--- components/auth/SignOut.tsx
+++ components/auth/SignOut.tsx
@@ -3,13 +3,21 @@
 import { LogOut } from "lucide-react";
 
 import { Button } from "../ui/button";
-import { signOut } from "next-auth/react";
+
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 
 export function SignOut() {
+  const supabase = createClient();
+  const router = useRouter();
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
   return (
     <Button
       className='w-full space-x-3 bg-destructive flex justify-start items-center gap-3 hover:bg-destructive/80'
-      onClick={() => signOut()}
+      onClick={() => handleSignOut()}
     >
       <LogOut /> <span>Sign Out</span>
     </Button>
