5b51779e436cdb8981c68c0991c58b304072b37e
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-09 15:01:21 -0700

    login pahe layout

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,5 +1,6 @@
 import { GoogleButton } from "@/components/auth/GoogleButton";
 import { LoginForm } from "@/components/auth/LoginForm";
+import { Separator } from "@/components/ui/separator";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
@@ -12,8 +13,9 @@ export default async function LoginPage() {
   if (isLoggedIn) redirect("/");
 
   return (
-    <div>
+    <div className='h-screen max-w-7xl mx-auto px-5 flex flex-col gap-8 justify-start items-center mt-5 sm:mt-10'>
       <LoginForm />
+      <Separator />
       <GoogleButton />
     </div>
   );
