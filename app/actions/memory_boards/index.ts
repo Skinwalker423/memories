c52d9e6ba3b82da652b7d3fa4f025f216c0025d8
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-10 12:27:41 -0700

    createBoardTitle

--- app/actions/memory_boards/index.ts
+++ app/actions/memory_boards/index.ts
@@ -2,9 +2,10 @@
 
 import { type MemoryBoard } from "@/app/types";
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export const fetchBoardById = async (id: string) => {
-  const supabase = await createClient();
+  const supabase = createClient();
   const { data: memory_board } = await supabase
     .from("memory_board")
     .select("*")
@@ -14,4 +15,27 @@ export const fetchBoardById = async (id: string) => {
   if (!memory_board) return null;
 
   return memory_board[0] as MemoryBoard;
+};
+
+export const createBoardTitle = async (title: string) => {
+  const supabase = createClient();
+  const currentUser = await supabase.auth.getUser();
+
+  if (!currentUser.data.user?.id) redirect("/login");
+
+  const { data, error } = await supabase
+    .from("memory_board")
+    .insert({
+      title,
+      userId: currentUser.data.user.id,
+      images: [],
+    });
+
+  if (error) {
+    return {
+      error: error.message,
+    };
+  }
+
+  redirect("/dashboard");
 };
