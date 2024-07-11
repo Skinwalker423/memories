ca44eb5251cf40d2fb695a648bfa3a9b0b0a8822
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-11 11:58:49 -0700

    dashboard page

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,7 +1,23 @@
+import { getCurrentUser } from "@/app/actions/users";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+const DashboardPage = async () => {
+  const user = await getCurrentUser();
+  if (!user) redirect("/login");
+
+  return (
+    <div className='min-h-screen w-full'>
+      <h1>Dashboard</h1>
+      <Button variant={"destructive"} asChild>
+        <Link href={"/dashboard/myBoards"}>
+          Create a Memory
+        </Link>
+      </Button>
+    </div>
+  );
 };
 
 export default DashboardPage;
