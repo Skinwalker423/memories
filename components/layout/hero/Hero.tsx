3bc555a4e2ba97dc1a2e03c5e79b96b5f1cceade
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-12 11:37:02 -0700

    secondary color amber

--- components/layout/hero/Hero.tsx
+++ components/layout/hero/Hero.tsx
@@ -9,10 +9,7 @@ const Hero = () => {
       <div className='absolute w-full h-full top-0 left-0 flex flex-col justify-center items-center gap-12 px-5'>
         <h1 className='max-w-6xl h-fit flex flex-wrap justify-start sm:justify-center  text-5xl sm:text-7xl md:text-8xl text-slate-50 font-bold z-50 text-left sm:text-center'>
           <span>Unleash Your &nbsp;</span>
-          <span className='text-yellow-500'>
-            {" "}
-            Memories.
-          </span>
+          <span className='text-secondary'> Memories.</span>
           <span>Create.</span>
           <span>Challenge.</span>
         </h1>
@@ -26,13 +23,13 @@ const Hero = () => {
         <div className='z-50 space-x-3'>
           <Link
             href='/boards'
-            className='bg-yellow-500 px-3 py-3 sm:px-4 rounded-lg text-yellow-50 text-base sm:text-xl'
+            className='bg-secondary px-3 py-3 sm:px-4 rounded-lg text-secondary-foreground text-base sm:text-xl'
           >
             Browse Memories
           </Link>
           <Link
             href='/dashboard'
-            className='border border-yellow-400 bg-transparent px-3 py-3 sm:px-4 rounded-lg text-white text-base sm:text-xl'
+            className='border border-secondary bg-transparent px-3 py-3 sm:px-4 rounded-lg text-secondary-foreground text-base sm:text-xl'
           >
             Create a memory
           </Link>
