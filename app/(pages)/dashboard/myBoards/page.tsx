967689db622a30e5b7c9dc69850dd248914f52b0
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-12 10:47:57 -0700

    tooltip for memory board status

--- app/(pages)/dashboard/myBoards/page.tsx
+++ app/(pages)/dashboard/myBoards/page.tsx
@@ -1,11 +1,20 @@
+import React from "react";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
 import { getAllUserBoards } from "@/app/actions/memory_boards";
 import { getCurrentUser } from "@/app/actions/users";
+
 import CreateBoardForm from "@/components/memory_game/CreateBoardForm";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import React from "react";
+import { CheckCircle, TriangleAlert } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const MyBoardsPage = async () => {
   const user = await getCurrentUser();
@@ -30,7 +39,40 @@ const MyBoardsPage = async () => {
                   className='bg-neutral-50 flex flex-col justify-between'
                   key={board.id}
                 >
-                  <h4>{board.title}</h4>
+                  <div className='flex items-center justify-between'>
+                    <h4>{board.title}</h4>
+                    <span>
+                      {board.images.length === 8 ? (
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <CheckCircle className='stroke-green-500' />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>
+                                Completed Memory. Ready to
+                                play.
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      ) : (
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <TriangleAlert className='stroke-yellow-500' />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>
+                                Not complete. Need to add
+                                more images
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      )}
+                    </span>
+                  </div>
                   <Image
                     src={board.images[0] || "/next.svg"}
                     alt={`image representing the memory board of ${board.title}`}
