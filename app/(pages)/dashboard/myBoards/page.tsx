4a9dcd5d27459be863e2c90f72de65b14dc8ef60
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-12 11:41:09 -0700

    layout

--- app/(pages)/dashboard/myBoards/page.tsx
+++ app/(pages)/dashboard/myBoards/page.tsx
@@ -23,73 +23,80 @@ const MyBoardsPage = async () => {
   const boards = await getAllUserBoards(user.id);
 
   return (
-    <div>
-      <h1>My Boards</h1>
-      <div>
-        <h2>Create Memory Project</h2>
+    <div className='max-w-6xl mx-auto'>
+      <h1 className='text-7xl text-center'>My Boards</h1>
+      <div className='my-20'>
         <CreateBoardForm />
       </div>
-      <div>
-        <h3>Memories</h3>
+      <div className='space-y-5'>
+        <h3 className='text-3xl font-semibold text-primary'>
+          Memories
+        </h3>
         <div>
-          <ul className='flex flex-wrap gap-3'>
-            {boards?.map((board) => {
-              return (
-                <li
-                  className='bg-neutral-50 flex flex-col justify-between'
-                  key={board.id}
-                >
-                  <div className='flex items-center justify-between'>
-                    <h4>{board.title}</h4>
-                    <span>
-                      {board.images.length === 8 ? (
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger>
-                              <CheckCircle className='stroke-green-500' />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p>
-                                Completed Memory. Ready to
-                                play.
-                              </p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      ) : (
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger>
-                              <TriangleAlert className='stroke-yellow-500' />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p>
-                                Not complete. Need to add
-                                more images
-                              </p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      )}
-                    </span>
-                  </div>
-                  <Image
-                    src={board.images[0] || "/next.svg"}
-                    alt={`image representing the memory board of ${board.title}`}
-                    width={200}
-                    height={200}
-                  />
-                  <Button asChild>
-                    <Link
-                      href={`/dashboard/myBoards/${board.id}`}
-                    >
-                      Add Images
-                    </Link>
-                  </Button>
-                </li>
-              );
-            })}
-          </ul>
+          {boards && (
+            <ul className='flex flex-wrap gap-10'>
+              {boards.map((board) => {
+                const isGameComplete =
+                  board.images.length === 8;
+                return (
+                  <li
+                    className='bg-neutral-50 flex flex-col justify-between'
+                    key={board.id}
+                  >
+                    <div className='flex items-center justify-between'>
+                      <h4>{board.title}</h4>
+                      <span>
+                        {isGameComplete ? (
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger>
+                                <CheckCircle className='stroke-green-500' />
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p>
+                                  Completed Memory. Ready to
+                                  play.
+                                </p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        ) : (
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger>
+                                <TriangleAlert className='stroke-yellow-500' />
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p>
+                                  Not complete. Need to add
+                                  more images
+                                </p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        )}
+                      </span>
+                    </div>
+                    <Image
+                      src={board.images[0] || "/next.svg"}
+                      alt={`image representing the memory board of ${board.title}`}
+                      width={200}
+                      height={200}
+                    />
+                    <Button asChild>
+                      <Link
+                        href={`/dashboard/myBoards/${board.id}`}
+                      >
+                        {isGameComplete
+                          ? "Customize"
+                          : "Add Images"}
+                      </Link>
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
         </div>
       </div>
     </div>
