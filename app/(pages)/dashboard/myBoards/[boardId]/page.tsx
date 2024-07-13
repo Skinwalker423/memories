3cd2d3230176ae55f1069130c56c8e0f4017e7c0
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-13 14:26:32 -0700

    edit board page

--- app/(pages)/dashboard/myBoards/[boardId]/page.tsx
+++ app/(pages)/dashboard/myBoards/[boardId]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchBoardById } from "@/app/actions/memory_boards";
 import { getCurrentUser } from "@/app/actions/users";
+import AddMemoryImageForm from "@/components/memory_game/AddMemoryImageForm";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ImageIcon } from "lucide-react";
@@ -26,62 +27,71 @@ const MyBoardPage = async ({
   console.log("is complete", isBoardComplete);
 
   return (
-    <div className='max-w-6xl mx-auto flex flex-col gap-10'>
-      <h1 className='text-8xl text-center'>
-        {board.title}
-      </h1>
-      <p
-        className={cn(
-          "px-4 py-6 w-full rounded-xl text-lg",
-          isBoardComplete ? "bg-green-400" : "bg-yellow-200"
-        )}
-      >
-        {isBoardComplete
-          ? "Completed board"
-          : `Add more images. Requires ${
-              8 - numberOfImages
-            } more before it is complete.`}
-      </p>
-      <div className=''>
-        <ul className='flex flex-wrap gap-8'>
-          {board.images.map((image, index) => {
-            return (
-              <li
-                key={`${index}-${image}`}
-                className='w-[200px] h-[200px] relative'
-              >
-                <Image
-                  src={image}
-                  alt={`image number ${index + 1} of ${
-                    board.title
-                  }`}
-                  fill
-                  className='object-cover'
-                />
-              </li>
-            );
-          })}
-          {!isBoardComplete && (
-            <div className='w-[200px] h-[200px] bg-neutral-200 flex flex-col justify-center items-center rounded-xl'>
-              <h3>Add Image</h3>
-              <ImageIcon size={100} />
-            </div>
+    <>
+      <div className='max-w-6xl mx-auto flex flex-col gap-10 h-full'>
+        <h1 className='text-8xl text-center'>
+          {board.title}
+        </h1>
+        <p
+          className={cn(
+            "px-4 py-6 w-full rounded-xl text-lg",
+            isBoardComplete
+              ? "bg-green-400"
+              : "bg-yellow-200"
           )}
-        </ul>
-      </div>
-      <div className='flex justify-end gap-5 items-center'>
-        {isBoardComplete && (
-          <Button asChild size={"lg"} variant={"secondary"}>
-            <Link href={`/boards/${board.id}`}>
-              View Game
-            </Link>
+        >
+          {isBoardComplete
+            ? "Completed board"
+            : `Add more images. Requires ${
+                8 - numberOfImages
+              } more before it is complete.`}
+        </p>
+        <div className=''>
+          <ul className='flex flex-wrap gap-8'>
+            {board.images.map((image, index) => {
+              return (
+                <li
+                  key={`${index}-${image}`}
+                  className='w-[200px] h-[200px] relative'
+                >
+                  <Image
+                    src={image}
+                    alt={`image number ${index + 1} of ${
+                      board.title
+                    }`}
+                    fill
+                    className='object-cover'
+                  />
+                </li>
+              );
+            })}
+            {!isBoardComplete && (
+              <AddMemoryImageForm userId={user.id} />
+              // <div className='w-[200px] h-[200px] bg-neutral-200 flex flex-col justify-center items-center rounded-xl'>
+              //   <h3>Add Image</h3>
+              //   <ImageIcon size={100} />
+              // </div>
+            )}
+          </ul>
+        </div>
+        <div className='flex justify-end gap-5 items-center'>
+          {isBoardComplete && (
+            <Button
+              asChild
+              size={"lg"}
+              variant={"secondary"}
+            >
+              <Link href={`/boards/${board.id}`}>
+                View Game
+              </Link>
+            </Button>
+          )}
+          <Button size={"lg"} variant={"destructive"}>
+            Delete Game
           </Button>
-        )}
-        <Button size={"lg"} variant={"destructive"}>
-          Delete Game
-        </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
