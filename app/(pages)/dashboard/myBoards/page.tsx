245ff0c5d8ed6a6401342ea172639daaab71a430
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-16 12:15:31 -0700

    alert for board limit

--- app/(pages)/dashboard/myBoards/page.tsx
+++ app/(pages)/dashboard/myBoards/page.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { redirect } from "next/navigation";
-import Link from "next/link";
-import Image from "next/image";
 
 import { getAllUserBoards } from "@/app/actions/memory_boards";
 import { getCurrentUser } from "@/app/actions/users";
 
 import CreateBoardForm from "@/components/memory_game/CreateBoardForm";
-import { Button } from "@/components/ui/button";
-import { CheckCircle, TriangleAlert } from "lucide-react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+
 import { MyBoardMemoryCard } from "@/components/cards/MyBoardMemoryCard";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
 
 const MyBoardsPage = async () => {
   const user = await getCurrentUser();
@@ -23,13 +22,36 @@ const MyBoardsPage = async () => {
 
   const boards = await getAllUserBoards(user.id);
 
+  const numberOfBoardsCreated = boards?.length || 0;
+
+  console.log(
+    "num boards created and allowed",
+    numberOfBoardsCreated
+  );
+
   return (
     <div className='max-w-6xl mx-auto p-10'>
       <h1 className='text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center'>
         My Boards
       </h1>
       <div className='my-20'>
-        <CreateBoardForm />
+        {numberOfBoardsCreated > 1 ? (
+          <Alert variant={"destructive"}>
+            <AlertCircle className='h-4 w-4' />
+            <AlertTitle>Heads up!</AlertTitle>
+            <AlertDescription>
+              <p className='w-full flex justify-between items-center'>
+                1 Memory board is allowed for all. To create
+                additional memories, see our specials!
+                <Button asChild>
+                  <Link href={"/purchase"}>View Deals</Link>
+                </Button>
+              </p>
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <CreateBoardForm />
+        )}
       </div>
       <div className='flex flex-col gap-4 justify-center'>
         <h3 className='text-3xl font-semibold text-'>
