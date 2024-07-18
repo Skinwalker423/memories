248ae9f1ff8c139987c1e98710b4d36f0826f171
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-07-18 12:08:21 -0700

    mobile side frame

---
+++ app/(pages)/dashboard/_components/MobileSideBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MobileSideBar = () => {
+  return (
+    <div className='flex fixed bottom-0 z-50 bg-red-300 sm:hidden w-full'>
+      Mobile Menu
+    </div>
+  );
+};
