0fa5b29e2eafec3ec4d581816049c8abd79dc042
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-09-13 14:21:45 -0700

    useSeachParams suspense boundaries

--- app/(pages)/auth/error/page.tsx
+++ app/(pages)/auth/error/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import {
-  useParams,
-  useSearchParams,
-} from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 enum Error {
@@ -33,29 +30,32 @@ const errorMap = {
   ),
 };
 
-export default function AuthErrorPage() {
+function Search() {
   const search = useSearchParams();
-  if (!search) return;
   const error = search.get("error") as Error;
 
-  if (!error) return;
-
   return (
-    <Suspense>
-      <div className='flex flex-col items-center justify-center w-full h-screen'>
-        <a
-          href='#'
-          className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center'
-        >
-          <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white flex flex-row justify-center items-center gap-2'>
-            Something went wrong
-          </h5>
-          <div className='font-normal text-gray-700 dark:text-gray-400'>
-            {errorMap[error] ||
-              "Please contact us if this error persists."}
-          </div>
-        </a>
-      </div>
-    </Suspense>
+    <div className='font-normal text-gray-700 dark:text-gray-400'>
+      {errorMap[error] ||
+        "Please contact us if this error persists."}
+    </div>
+  );
+}
+
+export default function AuthErrorPage() {
+  return (
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
+      <a
+        href='#'
+        className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center'
+      >
+        <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white flex flex-row justify-center items-center gap-2'>
+          Something went wrong
+        </h5>
+        <Suspense fallback={"Loading..."}>
+          <Search />
+        </Suspense>
+      </a>
+    </div>
   );
 }
